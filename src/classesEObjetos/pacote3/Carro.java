a1c7dfd38cfcf86dd4e1cf1535a07b88efb876ca
Maria Silva <maria.correia.s@gmail.com>
2023-03-04 18:43:25 -0300

    getters e setters

--- src/classesEObjetos/pacote3/Carro.java
+++ src/classesEObjetos/pacote3/Carro.java
@@ -4,17 +4,48 @@ public class Carro {
 
     public static final String VERMELHO = "vermelho";
     public static final String PRETO = "preto";
-    private final Integer quantidadePneu;
+    private Integer quantidadePneu;
+    private Integer quantidaDeCalotas;
+    private Integer quantidaDeParafusos;
+
 
     public Carro(Integer quantidadePneu){
+        setQuantidadePneu(quantidadePneu);
+    }
+
+    public Integer getQuantidadePneu() {
+        return quantidadePneu + 1;
+    }
+
+    public void setQuantidadePneu(Integer quantidadePneu) {
+        quantidaDeCalotas = quantidadePneu;
+        quantidaDeParafusos = quantidadePneu *4;
         this.quantidadePneu = quantidadePneu;
+    }
+
+    public Integer getQuantidaDeCalotas() {
+        return quantidaDeCalotas;
+    }
+
+    public void setQuantidaDeCalotas(Integer quantidaDeCalotas) {
+        this.quantidaDeCalotas = quantidaDeCalotas;
+    }
+
+    public Integer getQuantidaDeParafusos() {
+        return quantidaDeParafusos;
+    }
+
+    public void setQuantidaDeParafusos(Integer quantidaDeParafusos) {
+        this.quantidaDeParafusos = quantidaDeParafusos;
     }
 
     public void exibeCor(String cor){
         System.out.println(cor);
     }
 
-    public void exibeQuantidade(){
-        System.out.println("Quantidade de Pneus: "+ this.quantidadePneu);
+    public void imprimeValores(){
+        System.out.println("Quantidade de Pneus: "+ getQuantidadePneu());
+        System.out.println("Quantidade de Calotas: "+ getQuantidaDeCalotas());
+        System.out.println("Quantidade de Parafusos: "+ getQuantidaDeParafusos());
     }
 }
--- src/classesEObjetos/pacote3/ClassMain.java
+++ src/classesEObjetos/pacote3/ClassMain.java
@@ -5,7 +5,7 @@ public class ClassMain {
     public static void main(String[] args){
         Carro carro = new Carro(4);
         carro.exibeCor(Carro.PRETO);
-        carro.exibeQuantidade();
+        carro.imprimeValores();
     }
 
 }
