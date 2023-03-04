ef431044d1b3cd722d477e19bf3c0745aab9aa30
Maria Silva <maria.correia.s@gmail.com>
2023-03-04 16:59:32 -0300

    Aula introdutória Módulo 4

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import classesEObjetos.Introducao.Pessoa;
+
+public class Main {
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa("Lucas", "Santos");
+        pessoa1.setIdade(25);
+        System.out.println(pessoa1.getSaudacao());
+
+    }
+}
---
+++ src/classesEObjetos/Principal.java
@@ -0,0 +1,7 @@
+package classesEObjetos;
+
+public class Principal {
+    public Integer soma(Integer valor1, Integer valor2){
+        return valor1 +valor2;
+    }
+}
