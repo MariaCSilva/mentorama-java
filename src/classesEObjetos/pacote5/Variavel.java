92a8141f61aeb5006f57757c4ba273a21cfe2c19
Maria Silva <maria.correia.s@gmail.com>
2023-03-19 15:51:53 -0300

    Acessando conteúdo de um Wrapper nulo

--- src/classesEObjetos/pacote5/Variavel.java
+++ src/classesEObjetos/pacote5/Variavel.java
@@ -2,6 +2,12 @@ package classesEObjetos.pacote5;
 
 public class Variavel {
     public static void main(String[] args){
+        Variavel variavel =  new Variavel();
+        variavel.tipoInteiro();
+        variavel.gerarNullPointerException();
+    }
+
+    public void tipoInteiro(){
         Integer valor1 = 12; //Classe wrapper (Autoboxing)
         int valor2 = valor1; //Tipo primitivo (Auto-unboxing)
         System.out.println(valor1);
@@ -10,5 +16,11 @@ public class Variavel {
 
         System.out.println(valor1);
         System.out.println(valor2);
+    }
+
+    public void gerarNullPointerException(){
+        System.out.println("-------------");
+        Integer valor2 = null;
+        valor2.doubleValue();
     }
 }
