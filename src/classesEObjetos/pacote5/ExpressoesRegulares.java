01d5690c8865b6abe45ceb6d29fcf4ab193e57ab
Maria Silva <maria.correia.s@gmail.com>
2023-03-19 17:24:40 -0300

    Utilizando regex]

---
+++ src/classesEObjetos/pacote5/ExpressoesRegulares.java
@@ -0,0 +1,15 @@
+package classesEObjetos.pacote5;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExpressoesRegulares {
+    public static void main(String[] args){
+        String cpf = "111.111.111-00";
+
+        String regex = "\\b([0-9]{3})\\.([0-9]{3})\\.([0-9]{2})";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher match = pattern.matcher("111.111.111-00");
+        System.out.println(match.find());
+    }
+}
