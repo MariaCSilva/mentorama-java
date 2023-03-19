99515e0906ab5294300fc062941260d0a84ce58f
Maria Silva <maria.correia.s@gmail.com>
2023-03-19 17:11:52 -0300

    Usando métodos da classe Math

---
+++ src/classesEObjetos/pacote5/OperacoesMatematicas.java
@@ -0,0 +1,14 @@
+package classesEObjetos.pacote5;
+
+public class OperacoesMatematicas {
+    public static void main(String[] args){
+        System.out.println(Math.round(15.4));
+        System.out.println(Math.round(15.5));
+        System.out.println(Math.round(15.6));
+
+        System.out.println(Math.random());
+        System.out.println(Math.max(1,10));
+
+    }
+
+}
