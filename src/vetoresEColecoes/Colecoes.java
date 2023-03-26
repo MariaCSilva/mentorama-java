817bfcc60c6f30df0cf3070f6ce58131e27cc652
Maria Silva <maria.correia.s@gmail.com>
2023-03-26 19:19:14 -0300

    Manipulação de Arrays

--- src/vetoresEColecoes/Colecoes.java
+++ src/vetoresEColecoes/Colecoes.java
@@ -1,13 +1,13 @@
 package vetoresEColecoes;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Colecoes {
     public static void main(String[] args) {
         arrayList();
         linkedList();
+        hashSet();
+        manipularArrays();
     }
 
     public static void arrayList(){
@@ -20,6 +20,12 @@ public class Colecoes {
         System.out.println("LinkedList usando a Interface List");
         List<String> list = new LinkedList<>();
         manipularLista(list);
+    }
+
+    public static void hashSet(){
+        System.out.println("HashSet usando a Interface set");
+        Set<String> set = new HashSet<>();
+        manipularSet(set);
     }
 
     public static void manipularLista(List<String> colecao){
@@ -38,6 +44,38 @@ public class Colecoes {
         colecao.remove(0);
         for (int i = 0; i < colecao.size();i++){
             System.out.println(colecao.get(i));
+        }
+    }
+
+    public static void manipularSet(Set<String> colecao){
+        colecao.add("Carro");
+        colecao.add("Moto");
+        System.out.println(colecao.add("Barco"));
+        System.out.println(colecao.add("Carro"));
+
+        for (String veiculo: colecao){
+            System.out.println(veiculo);
+        }
+    }
+
+    public static void manipularArrays(){
+        System.out.println("Manipulando arrays");
+        int[] valores = new int[4];
+        valores[0] = 10;
+        valores[1] = 15;
+        valores[2] = 20;
+        valores[3] = 25;
+        for (int i = 0; i < valores.length; i++ ){
+            System.out.println(valores[i]);
+        }
+        int[] valoresReverso = new int[valores.length];
+        int posicaomaxima = valores.length - 1;
+        for (int i = posicaomaxima; i >= 0; i--){
+            valoresReverso[posicaomaxima - i] = valores[i];
+        }
+
+        for (int i = 0; i < valoresReverso.length; i++ ){
+            System.out.println(valoresReverso[i]);
         }
     }
 }
