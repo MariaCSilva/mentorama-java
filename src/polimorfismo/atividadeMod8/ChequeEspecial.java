1b5fac1a9f68be418b40e80e9c15ff0211090257
Maria Silva <maria.correia.s@gmail.com>
2023-05-15 21:53:03 -0300

    Atividade Modulo 8 Implementação do banco e iniciada a iteracao com a classe Entrada

---
+++ src/polimorfismo/atividadeMod8/ChequeEspecial.java
@@ -0,0 +1,50 @@
+package polimorfismo.atividadeMod8;
+
+public class ChequeEspecial {
+    private double saldoChequeEspecial;
+    private double limiteChequeEspecial;
+    private boolean habilitado;
+    private double consumoChequeEspecial;
+
+    public double getSaldoChequeEspecial() {
+        return saldoChequeEspecial;
+    }
+
+    public void setSaldoChequeEspecial(double saldoChequeEspecial) {
+        this.saldoChequeEspecial = saldoChequeEspecial;
+    }
+
+    public double getLimiteChequeEspecial() {
+        return limiteChequeEspecial;
+    }
+
+    public void setLimiteChequeEspecial(double limiteChequeEspecial) {
+        this.limiteChequeEspecial = limiteChequeEspecial;
+    }
+
+    public boolean isHabilitado() {
+        return habilitado;
+    }
+
+    public void setHabilitado(boolean habilitado) {
+        this.habilitado = habilitado;
+    }
+
+    public double getConsumoChequeEspecial() {
+        return consumoChequeEspecial;
+    }
+
+    public void setConsumoChequeEspecial(double consumoChequeEspecial) {
+        this.consumoChequeEspecial += consumoChequeEspecial;
+    }
+
+    @Override
+    public String toString() {
+        return "ChequeEspecial{" +
+                "saldoChequeEspecial=" + saldoChequeEspecial +
+                ", limiteChequeEspecial=" + limiteChequeEspecial +
+                ", habilitado=" + habilitado +
+                ", consumoChequeEspecial=" + consumoChequeEspecial +
+                '}';
+    }
+}
